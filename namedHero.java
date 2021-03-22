0e82b80719d47d4130fa9d69aa1ac38410bb3d27
efoley7 <79622034+efoley7@users.noreply.github.com>
2021-03-21 22:16:53 -0500

    class for named tree

---
+++ namedHero.java
@@ -0,0 +1,18 @@
+public class namedHero implements Comparable<namedHero>{
+    public String name;
+    SuperheroInterface hero;
+    public namedHero(SuperheroInterface hero){
+        this.name = hero.getSuperheroName();
+        this.hero = hero;
+    }
+
+
+    public int compareNames(String otherName) {
+        return this.name.compareTo(otherName);
+    }
+
+    @Override
+    public int compareTo(namedHero o) {
+        return this.name.compareTo(o.name);
+    }
+}
